59048972a98ea245c97ea34114eb6109e9a04fe4
Frederik Gelder <frederik.gelder@freenet.de>
2017-11-17 23:21:29 +0100

    adding nuget to mono install

--- Install/install-mono-complete.sh
+++ Install/install-mono-complete.sh
@@ -3,5 +3,5 @@
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb http://download.mono-project.com/repo/ubuntu xenial main" | sudo tee /etc/apt/sources.list.d/mono-official.list
 apt update
-apt install -y mono-complete
-apt install --only-upgrade -y mono-complete
+apt install -y mono-complete nuget
+apt install --only-upgrade -y mono-complete nuget
