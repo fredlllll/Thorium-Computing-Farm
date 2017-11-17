dcc2eb835ec8b0cc9a303689e7d772a488d8dd72
Frederik Gelder <frederik.gelder@freenet.de>
2017-11-17 13:45:25 +0100

    adding update script

--- update.sh
+++ update.sh
@@ -1,6 +1,13 @@
-git pull
+USER="thorium"
 
+su $USER <<EOM
+cd $PWD
+git fetch
+git pull
+git submodule update --init --recursive
+git submodule update --recursive --remote
 cd Source
 nuget restore
 msbuild
 cd ..
+EOM
