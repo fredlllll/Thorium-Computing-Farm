0375245fec3057fd3658f99e29a1c93366aa149d
Frederik Gelder <frederik.gelder@freenet.de>
2017-11-17 16:23:38 +0100

    install mono script

---
+++ Install/install-mono-complete.sh
@@ -0,0 +1,7 @@
+. ./check-root.sh
+
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb http://download.mono-project.com/repo/ubuntu xenial main" | sudo tee /etc/apt/sources.list.d/mono-official.list
+apt update
+apt install -y mono-complete
+apt install --only-upgrade -y mono-complete
