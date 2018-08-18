92fe99528c1bf542f635525620a555b73f48a7b9
Frederik Gelder <frederik.gelder@freenet.de>
2018-08-18 17:33:25 +0200

    removing console menu from server. it just clutters the code.

--- Source/Thorium.Server/Program.cs
+++ Source/Thorium.Server/Program.cs
@@ -14,7 +14,6 @@ namespace Thorium.Server
     class Program
     {
         static ThoriumServer server;
-        static ConsoleMenu menu;
         static void Main(string[] args)
         {
             Logging.Logging.SetupLogging();
@@ -23,66 +22,6 @@ namespace Thorium.Server
 
             server = new ThoriumServer();
             server.Start();
-
-            Thread.Sleep(1000);
-
-            //AddBlenderJob();
-
-            if(args.Contains("-menu"))
-            {
-                menu = new ConsoleMenu();
-                menu.AddMethod("stop", Stop);
-                menu.AddMethod("listjobs", ListJobs);
-                menu.AddMethod("help", Help);
-                Help(null);
-                menu.Run();
-            }
-        }
-
-        static void AddBlenderJob()
-        {
-            dynamic config = ConfigFile.GetConfig("thorium_server");
-
-            var client = new TCPServiceInvoker("localhost", config.ListeningPort);
-
-            JObject info = new JObject
-            {
-                [JobProperties.TaskProducerType] = "Thorium_Blender.BlenderTaskProducer",//typeof(BlenderTaskProducer).AssemblyQualifiedName,
-                [JobAndTaskProperties.ExecutionerType] = "Thorium_Blender.BlenderExecutioner",//typeof(BlenderExecutioner).AssemblyQualifiedName,
-                ["fileName"] = "sarfis_test.blend",
-                ["framesStart"] = 34,
-                ["framesEnd"] = 90,
-            };
-
-            JObject arg = new JObject
-            {
-                ["jobName"] = "test blender job",
-                ["jobInformation"] = info
-            };
-
-            client.Invoke(ServerControlCommands.AddJob, arg);
-        }
-
-        static void Stop(string[] args)
-        {
-            server.Stop();
-            menu.Stop();
-        }
-
-        static void ListJobs(string[] args)
-        {
-            Console.WriteLine("Jobs:");
-            /*foreach(var j in server.JobManager.Jobs) {
-                Console.WriteLine(j.Value.ID);
-            }*/
-        }
-
-        static void Help(string[] args)
-        {
-            Console.WriteLine("available commands:");
-            Console.WriteLine("Stop");
-            Console.WriteLine("ListTasks");
-            Console.WriteLine("Help");
         }
     }
 }
